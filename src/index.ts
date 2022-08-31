8101eec898a4a04aba11a336ecdeb6e55e97a4b2
Thien <>
2022-08-31 16:57:56 +0700

    add logger for useReducer HookAPI

--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,77 @@
 import { addPlugin, Flipper } from 'react-native-flipper';
 import * as dayjs from 'dayjs';
-
-export type Configuration = {
-  resolveCyclic?: boolean;
-  actionsBlacklist?: Array<string>;
-  stateWhitelist?: string[];
-};
-
-const defaultConfig: Configuration = {
-  resolveCyclic: false,
-  actionsBlacklist: [],
-};
+import { useCallback } from 'react';
 
 let currentConnection: Flipper.FlipperConnection | null = null;
 
-const error = {
-  NO_STORE: 'NO_STORE',
-};
-
-const createStateForAction = (state: any, config: Configuration) => {
-  return config.stateWhitelist
-    ? config.stateWhitelist.reduce(
-        (acc, stateWhitelistedKey) => ({
-          ...acc,
-          [stateWhitelistedKey]: state[stateWhitelistedKey],
-        }),
-        {},
-      )
-    : state;
-};
+const createStateForAction = (state: any) => state;
 
 // To initiate initial state tree
-const createInitialAction = (store: any, config: Configuration) => {
+const createInitialAction = (state: any) => {
   const startTime = Date.now();
-  let initState = store.getState();
 
-  if (config.resolveCyclic) {
-    const cycle = require('cycle');
-
-    initState = cycle.decycle(initState);
-  }
-
-  let state = {
+  const data = {
     id: startTime,
     time: dayjs(startTime).format('HH:mm:ss.SSS'),
-    took: `-`,
+    took: '-',
     action: { type: '@@INIT' },
-    before: createStateForAction({}, config),
-    after: createStateForAction(initState, config),
+    before: createStateForAction({}),
+    after: createStateForAction(state),
   };
 
-  currentConnection.send('actionInit', state);
+  if (currentConnection) {
+    currentConnection.send('actionInit', data);
+  }
+
 };
 
-const createDebugger =
-  (config = defaultConfig) =>
-  (store: any) => {
-    if (currentConnection == null) {
-      addPlugin({
-        getId() {
-          return 'flipper-plugin-redux-debugger';
-        },
-        onConnect(connection: any) {
-          currentConnection = connection;
+const processState = (state: any, action: any, next: any) => {
+  const startTime = Date.now();
+  if (currentConnection) {
+    const now = Date.now();
 
-          currentConnection.receive(
-            'dispatchAction',
-            (data: any, responder: any) => {
-              console.log('flipper redux dispatch action data', data);
-              // respond with some data
-              if (store) {
-                store.dispatch({ type: data.type, ...data.payload });
-
-                responder.success({
-                  ack: true,
-                });
-              } else {
-                responder.success({
-                  error: error.NO_STORE,
-                  message: 'store is not setup in flipper plugin',
-                });
-              }
-            },
-          );
-
-          createInitialAction(store, config);
-        },
-        onDisconnect() {
-          currentConnection = null;
-        },
-        runInBackground() {
-          return true;
-        },
-      });
-    } else {
-      createInitialAction(store, config);
-    }
-
-    return (next: any) => (action: { type: string }) => {
-      let startTime = Date.now();
-      let before = store.getState();
-      let result = next(action);
-      if (currentConnection) {
-        let after = store.getState();
-        let now = Date.now();
-        let decycledAction = null;
-
-        if (config.resolveCyclic) {
-          const cycle = require('cycle');
-
-          before = cycle.decycle(before);
-          after = cycle.decycle(after);
-          decycledAction = cycle.decycle(action);
-        }
-
-        let state = {
-          id: startTime,
-          time: dayjs(startTime).format('HH:mm:ss.SSS'),
-          took: `${now - startTime} ms`,
-          action: decycledAction || action,
-          before: createStateForAction(before, config),
-          after: createStateForAction(after, config),
-        };
-
-        const blackListed = !!config.actionsBlacklist?.some(
-          (blacklistedActionType) =>
-            action.type?.includes(blacklistedActionType),
-        );
-        if (!blackListed) {
-          currentConnection.send('actionDispatched', state);
-        }
-      }
-
-      return result;
+    const data = {
+      id: startTime,
+      time: dayjs(startTime).format('HH:mm:ss.SSS'),
+      took: `${now - startTime} ms`,
+      action,
+      before: createStateForAction(state),
+      after: createStateForAction(next),
     };
-  };
 
-export default createDebugger;
+    currentConnection.send('actionDispatched', data);
+  }
+};
+
+const flipperLogger = (reducer: any) => {
+  const reducerWithLogger = useCallback(
+    (state: any, action:any) => {
+      const next = reducer(state, action);
+      processState(state, action, next);
+      return next;
+    },
+    [reducer]
+  );
+
+  if (currentConnection == null) {
+    addPlugin({
+      getId() {
+        return 'flipper-plugin-redux-debugger';
+      },
+      onConnect(connection: any) {
+        currentConnection = connection;
+        createInitialAction(reducer);
+      },
+      onDisconnect() {
+        currentConnection = null;
+      },
+      runInBackground() {
+        return true;
+      },
+    });
+  }
+
+  return reducerWithLogger;
+};
+
+export default flipperLogger;
